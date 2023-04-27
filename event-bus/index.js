54af743a3c559741c252cc96f0b1e9dad74132c6
Cristian Salazar <cristiancasd@gmail.com>
2023-04-27 16:50:18 -0500

    event bus updated: To can use locally and in Kuberenetes

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,23 +6,34 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  //Add    EVENT - 'PointsAdded'
-  //Redeem EVENT - 'PointsRedeem'
-  //Query  EVENT - 'PointsAdded'
 
-  console.log('event recibido : ',event)
+  const URL_ADD= process.env.URL_SRV_ADD_POINTS 
+  ? process.env.URL_SRV_ADD_POINTS+"/events" 
+  : "http://localhost:8060/events" 
+
+  const URL_REDEEM= process.env.URL_SRV_REDEEM_POINTS 
+  ? process.env.URL_SRV_REDEEM_POINTS+"/events" 
+  : "http://localhost:8062/events" 
+
+  const URL_QUERY=  process.env.URL_SRV_QUERY 
+  ? process.env.URL_SRV_QUERY+"/events" 
+  : "http://localhost:8061/events" 
+
+
+  console.log('event recibido : ',process.env.URL_SRV_QUERY+"/events" ,event)
 
   //Redeem points
-  axios.post(process.env.URL_SRV_REDEEM_POINTS+"/events", event).catch((err) => {console.log(' Redeem 8062', err.message);});
+  axios.post(URL_REDEEM, event).catch((err) => {console.log(' Redeem 8062', err.message);});
 
   // add points
-  axios.post(process.env.URL_SRV_ADD_POINTS+"/events", event).catch((err) => {console.log(' add 8060', err.message);});
+  axios.post(URL_ADD, event).catch((err) => {console.log(' add 8060', err.message);});
 
   // Query points
-  axios.post(process.env.URL_SRV_QUERY+"/events", event).catch((err) => {console.log('query 8061', err.message);});
+  axios.post(URL_QUERY, event).catch((err) => {console.log('query 8061', err.message);});
 
   res.send({ status: "OK" });
 
